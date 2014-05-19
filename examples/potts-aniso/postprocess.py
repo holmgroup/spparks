9b7ae098e18b9247d70bcd616378ca9908ebcdf8
Brian DeCost <bdecost@andrew.cmu.edu>
2014-05-19 11:46:25 -0400

    working on example potts-aniso

--- examples/potts-aniso/postprocess.py
+++ examples/potts-aniso/postprocess.py
@@ -2,13 +2,39 @@
 
 import numpy as np
 import spktools.input as spkin
+from scipy.ndimage.filters import generic_filter
+
+def mode_filter(buffer):
+  """ return the most common value in the neighborhood """
+  return np.bincount(buffer.astype(np.int64)).argmax()
+
+def diff_filter(buffer):
+  return (buffer != buffer[4]).sum()
 
 infile = 'dump1.potts'
 nsites, x_dim, y_dim, z_dim, spins, pos = spkin.parse_potts(infile)
 dimensionality = spkin.get_dimensionality(x_dim, y_dim, z_dim)
 
+spins = spins.reshape(((x_dim[1]-x_dim[0]), (y_dim[1]-y_dim[0])))
+
+cross = np.array([[0,1,0],[1,1,1],[0,1,0]])
+X = np.array([[1,0,1],[0,1,0],[1,0,1]])
+square = np.array([[1,1,1],[1,1,1],[1,1,1]])
+filtered = generic_filter(spins, mode_filter, footprint=X, mode='wrap')
+
+grain_boundary = generic_filter(filtered, diff_filter, footprint=square, mode='wrap')
 
 import matplotlib.pyplot as plt
-plt.imshow(spins.reshape(((x_dim[1]-x_dim[0]), (y_dim[1]-y_dim[0]))), interpolation='None')
+plt.imshow(spins, interpolation='None')
 plt.colorbar()
+plt.show()
+
+plt.imshow(filtered, interpolation='None')
+plt.colorbar()
+plt.show()
+
+microstructure = filtered
+microstructure[grain_boundary > 2 ] = 0
+
+plt.imshow(microstructure, interpolation='None')
 plt.show()
--- examples/potts-aniso/preprocess.py
+++ examples/potts-aniso/preprocess.py
@@ -9,18 +9,22 @@ infile = 'init1.potts'
 outfile = 'input.potts'
 nsites, x_dim, y_dim, z_dim, spins, pos = spkin.parse_potts(infile)
 dimensionality = spkin.get_dimensionality(x_dim, y_dim, z_dim)
+print('loaded data')
 
 spkspec.create_special_grain(spins, pos,
                              x_dim[1]-x_dim[0],
                              y_dim[1]-y_dim[0],
                              z_dim[1]-z_dim[0],
                              size_advantage=6)
+print('created special grain')
 
 spin_map = spkspec.special_spin_map(spins)
 
+print('created spin map')
 
 high_fraction = 0.2
 ori = spktex.random_two_tone(spin_map, high_fraction)
+print('created texture map')
 
 mtab_file = 'potts-test.mtab'
 spkspec.write_mtab(ori, mtab_file, 1000)
@@ -28,17 +32,17 @@ spkspec.write_mtab(ori, mtab_file, 1000)
 with open(outfile, 'w') as of:
   spkin.print_header(of, dimensionality, nsites, x_dim, y_dim, z_dim)
   spkin.print_Sites(of, spins, spin_map)
+print('wrote spin file')
 
+# colors = np.copy(spins)
+# for i in np.arange(spins.size):
+#   if spins[i] == 1:
+#     colors[i] = 0
+#   else:
+#     colors[i] = ori[spin_map[spins[i]]]
 
-colors = np.copy(spins)
-for i in np.arange(spins.size):
-  if spins[i] == 1:
-    colors[i] = 0
-  else:
-    colors[i] = ori[spin_map[spins[i]]]
-
-print(len(spin_map))
-import matplotlib.pyplot as plt
-plt.imshow(colors.reshape(((x_dim[1]-x_dim[0]), (y_dim[1]-y_dim[0]))), interpolation='None')
-plt.colorbar()
-plt.show()
+# print(len(spin_map))
+# import matplotlib.pyplot as plt
+# plt.imshow(colors.reshape(((x_dim[1]-x_dim[0]), (y_dim[1]-y_dim[0]))), interpolation='None')
+# plt.colorbar()
+# plt.show()
